1c6e0fd3ba19ad158d372139eab1e2f704aaf914
Philip Trostli <philiptrostli@gmail.com>
2023-01-03 18:50:22 -0500

    db temp fix with heroes

--- db/migrate/20230101230800_create_heros.rb
+++ db/migrate/20230101230800_create_heros.rb
@@ -1,6 +1,6 @@
-class CreateHeroes < ActiveRecord::Migration[5.2]
+class CreateHeros < ActiveRecord::Migration[5.2]
   def change
-    create_table :heroes do |t|
+    create_table :heros do |t|
       t.string :name, null:false
       t.string :health, null:false
       t.string :shield_hp
